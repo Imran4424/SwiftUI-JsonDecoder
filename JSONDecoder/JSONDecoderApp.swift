3b97219305ad7420d2f89b19fb959a401d9b5fef
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-15 04:05:16 +0600

    project init

---
+++ JSONDecoder/JSONDecoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONDecoderApp.swift
+//  JSONDecoder
+//
+//  Created by Shah Md Imran Hossain on 15/4/23.
+//
+
+import SwiftUI
+
+@main
+struct JSONDecoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

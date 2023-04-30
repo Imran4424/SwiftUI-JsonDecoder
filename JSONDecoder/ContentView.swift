aa15964aaf009be5725ba6417afa9b92b60e856e
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-30 20:33:31 +0600

    with closure image now frame works

--- JSONDecoder/ContentView.swift
+++ JSONDecoder/ContentView.swift
@@ -25,47 +25,19 @@ class User: ObservableObject, Codable {
     }
 }
 
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
-struct Response: Codable {
-    var results: [Result]
-}
-
 struct ContentView: View {
-//    @State private var results = [Result]()
-    
     var body: some View {
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"))
-            .frame(width: 200, height: 200) // frame will not work for async image
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+            Color.green
+        }
+        .frame(width: 200, height: 200)
     }
 }
 
-// MARK: - Async Methods
-extension ContentView {
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            guard let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) else {
-                print("Invalid decoded response")
-                return
-            }
-            
-//            results = decodedResponse.results
-        } catch {
-            print("Invalid data")
-        }
-    }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

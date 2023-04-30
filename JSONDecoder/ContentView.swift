c6b6db820f5524484979a7f0228929ae054e9295
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-30 20:00:08 +0600

    decode json from server

--- JSONDecoder/ContentView.swift
+++ JSONDecoder/ContentView.swift
@@ -25,11 +25,53 @@ class User: ObservableObject, Codable {
     }
 }
 
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+struct Response: Codable {
+    var results: [Result]
+}
+
 struct ContentView: View {
+    @State private var results = [Result]()
+    
     var body: some View {
-        Text("Soudha")
-            .font(.largeTitle)
-            .foregroundColor(.blue)
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }
+        .task {
+            await loadData()
+        }
+    }
+}
+
+// MARK: - Async Methods
+extension ContentView {
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            guard let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) else {
+                print("Invalid decoded response")
+                return
+            }
+            
+            results = decodedResponse.results
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
